55c66a942636ef813a852e91819b557b694880ba
Michael Howard <mphoward@auburn.edu>
2023-03-01 14:58:02 -0600

    Record usage in git repository

--- usage_report/usage_report.sh
+++ usage_report/usage_report.sh
@@ -5,7 +5,24 @@ if [ $# -lt 1 ]; then
     exit 1
 fi
 
+# generate the new report
 module load python/3.8.6
 source env/bin/activate
-
 python3 usage_report.py -p chen_std chen_bg2 -m $1 -o ${1}.md
+
+# prepend the report to the history
+cat <<EOF >>history.md.tmp
+# Usage history
+
+## ${1}
+
+EOF
+cat ${1}.md >> history.md.tmp
+if [ -f history.md ];
+then
+    tail -n +3 history.md >> history.md.tmp
+fi
+mv history.md.tmp history.md
+
+# don't keep the individual month's report anymore
+rm ${1}.md
